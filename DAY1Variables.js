f9ee984edd581e12017655b67ec292afb3226bd4
Rahul Rana <113045763+RahulKumarRana@users.noreply.github.com>
2025-06-12 18:23:59 +0000

    Variables

---
+++ DAY1Variables.js
@@ -0,0 +1,8 @@
+let name="rahul rana"
+const phoneNo=95081816218
+const Address = "Aadarsh mohalla Koderma"
+let Postiion="developer"
+console.log(name,phoneNo);
+console.table([name,phoneNo,Address,Postiion])
+
+console.log(typeof name," ", typeof phoneNo)
